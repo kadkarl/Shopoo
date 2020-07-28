554a57074dc9ccd3104c7b13760e906a74c3065e
kadkarl <abkder@gmail.com>
2020-07-28 11:24:33 +0200

    Projet initial

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,11 +8,13 @@ namespace Shopoo.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
         }
 
+        [AllowAnonymous]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
