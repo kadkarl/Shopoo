8572ded49a5a2235433985583768a511f7476137
kadkarl <abkder@gmail.com>
2020-07-28 11:39:01 +0200

    Projet initial

---
+++ Models/Panier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Shopoo.Models
+{
+    public class Panier
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public virtual ICollection<Produit> Produits { get; set; }
+
+        public virtual Utilisateur Utilisateur { get; set; }
+
+    }
+}
